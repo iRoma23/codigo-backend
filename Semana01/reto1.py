c49c855671ed71e9465786efc03d8e23bb42b000
Romario <romario.jara.ch@gmail.com>
2022-04-03 15:03:15 -0500

    Semana 01 - reto 1

--- Semana01/reto1.py
+++ Semana01/reto1.py
@@ -1,1 +1,22 @@
-print("Hola")
+import time
+
+nombre = input("Ingrese su nombre ")
+edad = int(input("Ingrese su edad "))
+
+# Obtiene la hora actual
+def get_hour():
+  date = time.ctime()
+  date = date.split()
+  date_hour = date[3].split(":")
+  hour = date_hour[0]
+  return int(hour)
+
+current_hour = get_hour()
+
+if edad < 18:
+  if current_hour <= 18:
+    print(nombre + " debes realizar tus tareas.")
+  else:
+    print(nombre + " debes ir a dormir.")
+else:
+  print(nombre + " no estas obligado a hacer nada.")
